950302d3843630f72b6a5463065aa401a93fad25
James Rice <james@2b.co.uk>
2022-11-14 16:43:20 +0000

    add field to variant node

---
+++ gatsby-node.js
@@ -0,0 +1,13 @@
+exports.onCreateNode = ({ node, actions, getNode }) => {
+  const { createNodeField } = actions
+  console.log("node.internal.type: ", node.internal.type)
+  if (node.internal.type === `ShopifyProductVariant`) {
+    createNodeField({
+      node,
+      name: `Hello`,
+      value: {
+        example: "World!",
+      },
+    })
+  }
+}
